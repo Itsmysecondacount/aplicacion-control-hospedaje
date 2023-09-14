b44e744ecf8473ee0ef04c7a58eb69e17bd2e383
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-09-14 17:47:34 -0500

    backend

--- backend/crud.py
+++ backend/crud.py
@@ -69,6 +69,8 @@ def create_reporte(db: Session, reporte: models.ReportesModel):
 
 #Todos los métodos para recuperar datos
 
+#Tabla Clientes
+
 def get_cliente(db: Session, cliente_id: int):
     return db.query(database.Cliente).filter(database.Cliente.ClienteID == cliente_id).first()
 
@@ -76,4 +78,66 @@ def get_clientes(db: Session, skip: int = 0, limit: int = 10):
     return db.query(database.Cliente).offset(skip).limit(limit).all()
 
 def search_cliente_by_name(db: Session, name: str):
-    return db.query(database.Cliente).filter(database.Cliente.Nombre.ilike(f"%{name}%")).all()
+    return db.query(database.Cliente).filter(database.Cliente.Nombre.ilike(f"%{name}%")).limit(10).all()
+
+#Tabla para Personal
+# Recuperar un registro específico
+def get_personal(db: Session, personal_id: int):
+    return db.query(database.Personal).filter(database.Personal.PersonalID == personal_id).first()
+
+# Recuperar todos los registros ordenados por fecha
+def get_all_personal(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(database.Personal).offset(skip).limit(limit).all()
+
+#Tabla para Tareas
+# Recuperar un registro específico
+def get_tarea(db: Session, tarea_id: int):
+    return db.query(database.Tarea).filter(database.Tarea.TareaID == tarea_id).first()
+
+# Recuperar todos los registros ordenados por fecha
+def get_all_tareas(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(database.Tarea).offset(skip).limit(limit).all()
+
+##Agregar los métodos para tareas según el tipo "estado" completado o no
+
+# DetallesReporte
+def get_detalle_reporte(db: Session, detalle_reporte_id: int):
+    return db.query(database.DetallesReporte).filter(database.DetallesReporte.DetalleID == detalle_reporte_id).first()
+
+def get_all_detalles_reporte(db: Session):
+    return db.query(database.DetallesReporte).all()
+
+# Facturas
+def get_factura(db: Session, factura_id: int):
+    return db.query(database.Facturas).filter(database.Facturas.FacturaID == factura_id).first()
+
+def get_all_facturas_ordered_by_date(db: Session):
+    return db.query(database.Facturas).order_by(database.Facturas.Fecha).all()
+
+# Habitaciones
+def get_habitacion(db: Session, habitacion_id: int):
+    return db.query(database.Habitaciones).filter(database.Habitaciones.HabitacionID == habitacion_id).first()
+
+def get_all_habitaciones(db: Session):
+    return db.query(database.Habitaciones).all()
+
+# Pagos
+def get_pago(db: Session, pago_id: int):
+    return db.query(database.Pagos).filter(database.Pagos.PagoID == pago_id).first()
+
+def get_all_pagos_ordered_by_date(db: Session):
+    return db.query(database.Pagos).order_by(database.Pagos.Fecha).all()
+
+# Reportes
+def get_reporte(db: Session, reporte_id: int):
+    return db.query(database.Reportes).filter(database.Reportes.ReporteID == reporte_id).first()
+
+def get_all_reportes_ordered_by_date(db: Session):
+    return db.query(database.Reportes).order_by(database.Reportes.FechaGenerado).all()
+
+# Reservas
+def get_reserva(db: Session, reserva_id: int):
+    return db.query(database.Reservas).filter(database.Reservas.ReservaID == reserva_id).first()
+
+def get_all_reservas_ordered_by_date(db: Session):
+    return db.query(database.Reservas).order_by(database.Reservas.FechaInicio).all()
--- backend/main.py
+++ backend/main.py
@@ -54,6 +54,7 @@ def create_reporte(reporte: models.ReportesModel, db: Session = Depends(get_db))
 
 #Endpoints para recuperar datos
 
+#Tabla Clientes
 @app.get("/clientes/{cliente_id}")
 def read_cliente(cliente_id: int, db: Session = Depends(get_db)):
     db_cliente = crud.get_cliente(db, cliente_id=cliente_id)
@@ -70,3 +71,80 @@ def read_clientes(skip: int = 0, limit: int = 10, db: Session = Depends(get_db))
 def search_cliente(name: str, db: Session = Depends(get_db)):
     clientes = crud.search_cliente_by_name(db, name)
     return clientes
+
+#Tabla para Personal
+@app.get("/personal/{personal_id}")
+def read_personal(personal_id: int, db: Session = Depends(get_db)):
+    personal = crud.get_personal(db, personal_id)
+    return personal
+
+@app.get("/personal/")
+def read_all_personal(db: Session = Depends(get_db)):
+    personal_all = crud.get_all_personal(db)
+    return personal_all
+
+#Tabla Tareas
+@app.get("/tareas/{tarea_id}")
+def read_tarea(tarea_id: int, db: Session = Depends(get_db)):
+    tarea = crud.get_tarea(db, tarea_id)
+    return tarea
+
+@app.get("/tareas/")
+def read_all_tareas(db: Session = Depends(get_db)):
+    tareas = crud.get_all_tareas(db)
+    return tareas
+
+# Para DetallesReporte
+@app.get("/detalles_reporte/{detalle_reporte_id}")
+def read_detalle_reporte(detalle_reporte_id: int, db: Session = Depends(get_db)):
+    return crud.get_detalle_reporte(db, detalle_reporte_id)
+
+@app.get("/detalles_reporte/")
+def read_all_detalles_reporte(db: Session = Depends(get_db)):
+    return crud.get_all_detalles_reporte(db)
+
+# Para Facturas
+@app.get("/facturas/{factura_id}")
+def read_factura(factura_id: int, db: Session = Depends(get_db)):
+    return crud.get_factura(db, factura_id)
+
+@app.get("/facturas/")
+def read_all_facturas(db: Session = Depends(get_db)):
+    return crud.get_all_facturas_ordered_by_date(db)
+
+# Para Habitaciones
+@app.get("/habitaciones/{habitacion_id}")
+def read_habitacion(habitacion_id: int, db: Session = Depends(get_db)):
+    return crud.get_habitacion(db, habitacion_id)
+
+@app.get("/habitaciones/")
+def read_all_habitaciones(db: Session = Depends(get_db)):
+    return crud.get_all_habitaciones(db)
+
+# Para Pagos
+@app.get("/pagos/{pago_id}")
+def read_pago(pago_id: int, db: Session = Depends(get_db)):
+    return crud.get_pago(db, pago_id)
+
+@app.get("/pagos/")
+def read_all_pagos(db: Session = Depends(get_db)):
+    return crud.get_all_pagos_ordered_by_date(db)
+
+# Para Reportes
+@app.get("/reportes/{reporte_id}")
+def read_reporte(reporte_id: int, db: Session = Depends(get_db)):
+    return crud.get_reporte(db, reporte_id)
+
+@app.get("/reportes/")
+def read_all_reportes(db: Session = Depends(get_db)):
+    return crud.get_all_reportes_ordered_by_date(db)
+
+# Para Reservas
+@app.get("/reservas/{reserva_id}")
+def read_reserva(reserva_id: int, db: Session = Depends(get_db)):
+    return crud.get_reserva(db, reserva_id)
+
+@app.get("/reservas/")
+def read_all_reservas(db: Session = Depends(get_db)):
+    return crud.get_all_reservas_ordered_by_date(db)
+    
