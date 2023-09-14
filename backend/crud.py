939d71ebd33281901216c1bb559f44ed540de0c7
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-09-14 13:46:53 -0500

    backend

--- backend/crud.py
+++ backend/crud.py
@@ -7,4 +7,4 @@ def create_cliente(db: Session, cliente: models.Cliente):
     db.add(db_cliente)
     db.commit()
     db.refresh(db_cliente)
-    return models.Cliente(**db_cliente.dict())
+    return models.Cliente(**db_cliente.__dict__)
