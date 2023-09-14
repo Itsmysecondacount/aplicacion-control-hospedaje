12f6ddfe6dffd5c93f6c7271050e1282385dce31
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-09-14 13:19:51 -0500

    backend

--- backend/crud.py
+++ backend/crud.py
@@ -3,9 +3,8 @@ import models
 import database
 
 def create_cliente(db: Session, cliente: models.Cliente):
-    copy_client = cliente
     db_cliente = database.Cliente(**cliente.dict())
     db.add(db_cliente)
     db.commit()
-    db.refresh(cliente)
-    return copy_client
+    db.refresh(db_cliente)
+    return cliente
